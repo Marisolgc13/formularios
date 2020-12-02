4bb114a92d79b978479837637136972bf51c1d25
Marisolgc13 <64939683+Marisolgc13@users.noreply.github.com>
2020-12-01 19:30:52 -0700

    Create navbar.blade.php

---
+++ incluye/navbar.blade.php
@@ -0,0 +1,26 @@
+<div>
+@section('navbar')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-light" style="background-color: black;">
+  <div class="container">
+    <div class="navbar-header">
+    </div>
+    <ul class="nav navbar-nav" >
+      
+  </div>
+</nav>
+</body>
+</html>
+
+</div>
